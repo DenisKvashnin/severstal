1fd9c2c270795dd4c3d3b8e73df0c17211dee86b
klevita <kklevita92@gmail.com>
2023-02-17 23:46:50 +0300

    exhauster page

--- src/pages/ExhausterPage.vue
+++ src/pages/ExhausterPage.vue
@@ -1,17 +1,184 @@
 <template>
   <div style="position: relative;">
+
+
+    <div style="left:49.6%;top:16%;width:10.19%" class="liquid-measure exgauster-scheme-text">
+      12 %
+    </div>
+
+    <div style="left:74.7%;top:30.3%;width: 11.8%" class="liquid-measure exgauster-scheme-text">
+      12 кг/см2
+    </div>
+
+
+    <div style="left:71.65%;top:3.87%;" class="exgauster-scheme-text">
+      {{}}000 °C
+    </div>
+    <div style="left:67.1%;top:3.87%;" class="exgauster-scheme-text">
+      {{}}000 °C
+    </div>
+    <div style="left:62.65%;top:15.87%;" class="exgauster-scheme-text">
+      {{}}000 °C
+    </div>
+    <div style="left:69.4%;top:25.7%;" class="exgauster-scheme-text">
+      {{}}000 °C
+    </div>
+
+
+    <div style="left:72.15%;top:43.6%;" class="exgauster-scheme-text">
+      {{}}000
+    </div>
+    <div style="left:72.15%;top:46.6%;" class="exgauster-scheme-text">
+      {{}}000
+    </div>
+    <div style="left:72.15%;top:49.6%;" class="exgauster-scheme-text">
+      {{}}000
+    </div>
+    <div style="left:72.15%;top:52.6%;" class="exgauster-scheme-text">
+      {{}}000
+    </div>
+
+
     <div style="left:25%;top:24.3%;" class="exgauster-scheme-text">
       {{}}45 °C
     </div>
-    <div style="left:23.9%;top:28.3%;" class="text-white exgauster-scheme-text">
+    <div style="left:23.9%;top:28.3%;" class="exgauster-scheme-text">
       Разряжение, мм.в.ст &nbsp; {{}}74.3
     </div>
-    <div style="left:23.9%;top:31.2%;" class="text-white exgauster-scheme-text">
+    <div style="left:23.9%;top:31.2%;" class="exgauster-scheme-text">
       Уровень пыли, мг/м3 <span style="margin-left: 2px;">{{}}15</span>
     </div>
-    <div style="left:23.9%;top:31.9%;" class="text-white exgauster-scheme-text">
-      9 ПС <span style="margin-left: 2px;">{{}}15</span>
+
+
+    <div style="left:15%;top:53.8%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      9 ПС
     </div>
+    <div style="left:15%;top:57.2%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+
+
+    <div style="left:15%;top:66.3%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      8 ПС
+    </div>
+    <div style="left:15%;top:69.6%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+    <div style="left:15%;top:72.6%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>B, мм/с</div>
+      <div>8{{}}</div>
+    </div>
+    <div style="left:15%;top:75.6%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>Г, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+    <div style="left:15%;top:78.6%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>O, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+
+
+    <div style="left:39.4%;top:31.18%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      4 ПС
+    </div>
+    <div style="left:39.4%;top:34.8%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+
+
+    <div style="left:39.4%;top:68.0%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      6 ПС
+    </div>
+    <div style="left:39.5%;top:71.6%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>B, мм/с</div>
+      <div>8{{}}</div>
+    </div>
+
+
+    <div style="left:39.4%;top:78.2%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      7 ПС
+    </div>
+    <div style="left:39.5%;top:81.7%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+    <div style="left:39.5%;top:84.7%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>B, мм/с</div>
+      <div>8{{}}</div>
+    </div>
+    <div style="left:39.5%;top:87.7%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>Г, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+    <div style="left:39.5%;top:90.7%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>O, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+
+
+    <div style="left:48.1%;top:31.1%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      3 ПС
+    </div>
+    <div style="left:48.13%;top:34.8%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+
+
+    <div style="left:48.1%;top:68%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      5 ПС
+    </div>
+    <div style="left:48.2%;top:71.6%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+
+
+    <div style="left:56.4%;top:68%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      2 ПС
+    </div>
+    <div style="left:56.4%;top:71.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+    <div style="left:56.4%;top:74.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>B, мм/с</div>
+      <div>8{{}}</div>
+    </div>
+    <div style="left:56.4%;top:77.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>Г, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+    <div style="left:56.4%;top:80.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>O, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+
+
+    <div style="left:74.2%;top:68%;width:5.3%" class="row justify-center exgauster-scheme-text">
+      1 ПС
+    </div>
+    <div style="left:74.2%;top:71.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>T, °C</div>
+      <div>220{{}}</div>
+    </div>
+    <div style="left:74.2%;top:74.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>B, мм/с</div>
+      <div>8{{}}</div>
+    </div>
+    <div style="left:74.2%;top:77.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>Г, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+    <div style="left:74.2%;top:80.4%;width: 5.3%;" class="highlighted-val exgauster-scheme-text">
+      <div>O, мм/с</div>
+      <div>00{{}}</div>
+    </div>
+
+
     <q-img src="~assets/exgauster-scheme.svg" />
   </div>
 </template>
@@ -23,5 +190,23 @@
   font-weight: 600;
   font-size: 0.7vw;
   position: absolute;
+  color: white;
+
+
+}
+
+.highlighted-val {
+  display: flex !important;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: $warning-accent;
+  border-radius: 2px;
+  padding: 0 4px 0 4px;
+}
+
+.liquid-measure {
+  background-color: $secondary;
+  height: 20px;
+  padding-left: 3px;
 }
 </style>
